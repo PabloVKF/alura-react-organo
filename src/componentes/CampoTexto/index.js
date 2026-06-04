6c3e668cd71ca49a6810affab981caab9bbadaa4
Pablo Von Knoblauch Fontão <pablovkf@gmail.com>
2026-06-04 14:59:42 -0300

    feat: complete chapter 3

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,14 @@ import './CampoTexto.css';
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`;
 
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={placeholderModificada} />
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} type="text" placeholder={placeholderModificada} />
         </div>
     );
 }
